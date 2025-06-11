febc7daa6801e198647c73296b0ed5b590dd9c4e
“Ashish <chaurasiyaashish007@gmail.com>
2025-06-11 18:14:54 +0530

    Added config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,20 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
 });
